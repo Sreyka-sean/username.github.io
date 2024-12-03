4b35747a19f40dc1e3e69c7d929e037c268b0606
Sreyka-sean <sreykasean26@gmail.com>
2024-12-03 08:22:08 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.getElementById('contactButton').addEventListener('click', function() {
     alert('Thanks for reaching out! I will get back to you as soon as possible.');
 });
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((registration) => {
+          console.log("Service Worker registered:", registration);
+        })
+        .catch((error) => {
+          console.error("Service Worker registration failed:", error);
+        });
+    });
+  }
+  
